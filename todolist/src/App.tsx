d3915199d3009d5ef9a465cd9bf9a255ac867dae
Jason Ching Yuen Siu <sjsa3a28@gmail.com>
2024-01-21 22:50:31 +1100

    be: Part 2 - Adding a Todo item via a form

--- todolist/src/App.tsx
+++ todolist/src/App.tsx
@@ -8,9 +8,33 @@ function App() {
     { id: 2, title: "bar", completed: true },
   ]);
 
+  const [newTitle, setNewTitle] = useState("");
+
+  function addTodo(title: string) {
+    let maxId = 0;
+    for (let todo of todos) {
+      maxId = Math.max(maxId, todo.id);
+    }
+    const newTodo = { id: maxId + 1, title: title, completed: false };
+    setTodos([...todos, newTodo]);
+  }
+
   return (
     <div className="App">
       <h1>To Do</h1>
+      <form
+        onSubmit={(event) => {
+          event.preventDefault();
+          addTodo(newTitle);
+          setNewTitle("");
+        }}
+      >
+        <input
+          value={newTitle}
+          onChange={(event) => setNewTitle(event.target.value)}
+        />
+        <button type="submit">Add</button>
+      </form>
       <ul className="TodoList">
         {todos.map((todo) => (
           <li
